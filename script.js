279aadae77920cc520927a449d8cd70674c09b05
Israel Fleetwood <67342332+hgsisrael@users.noreply.github.com>
2021-07-19 20:52:46 +0000

    fisnished

--- script.js
+++ script.js
@@ -44,7 +44,14 @@ function setup() {
   };
   model = ml5.neuralNetwork(options);
   // new code below
-
+  let modelInfo = {
+    model: "model/model.json",
+    metadata: "model/model_meta.json"
+    weights: "model/model.weight.bin"
+  };
+  model.load(modelInfo, modelLoaded);
+  createMusicSystem();
+  }
   createMusicSystem();
 }
 
